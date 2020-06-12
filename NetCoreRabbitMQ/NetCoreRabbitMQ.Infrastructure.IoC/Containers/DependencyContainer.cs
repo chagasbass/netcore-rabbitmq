7b72e055d35c5454261a18bb86a85dd41d12ce2c
Thiago Chagas <chagas.bass@gmail.com>
2020-06-12 15:00:43 -0300

    criando a classe para resolucao de dependencias da aplicacao

---
+++ NetCoreRabbitMQ/NetCoreRabbitMQ.Infrastructure.IoC/Containers/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetCoreRabbitMQ.Domain.Core.Bus;
+using NetCoreRabbitMQ.Infrastructure.Bus.Bus;
+
+namespace NetCoreRabbitMQ.Infrastructure.IoC.Containers
+{
+    public static class DependencyContainer
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
