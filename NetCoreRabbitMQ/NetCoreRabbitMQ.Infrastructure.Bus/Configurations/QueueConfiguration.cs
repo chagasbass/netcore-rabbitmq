4d71ba62bc966fdc9df79f5ade51adeaa07c4291
Thiago Chagas <chagas.bass@gmail.com>
2020-06-12 15:08:15 -0300

    criando rotinas de alteracao de configuracao de fila na classe de configuracao de filas

--- NetCoreRabbitMQ/NetCoreRabbitMQ.Infrastructure.Bus/Configurations/QueueConfiguration.cs
+++ NetCoreRabbitMQ/NetCoreRabbitMQ.Infrastructure.Bus/Configurations/QueueConfiguration.cs
@@ -7,9 +7,14 @@ namespace NetCoreRabbitMQ.Infrastructure.Bus.Configurations
     /// </summary>
     public static class QueueConfiguration
     {
-     public static bool IsDurable { get; private set; } = false;
-     public static bool IsExclusive { get; private set; } = false;
-     public static bool IsAutoDelete { get; private set; } = false;
-     public static IDictionary<string,object>  Arguments { get; private set; } = null;
+        public static bool IsDurable { get; private set; } = false;
+        public static bool IsExclusive { get; private set; } = false;
+        public static bool IsAutoDelete { get; private set; } = false;
+        public static IDictionary<string, object> Arguments { get; private set; } = null;
+
+        public static void ChangeIsDurable(bool isDurable) => IsDurable = isDurable;
+        public static void ChangeIsExclusive(bool isExclusive) => IsExclusive = isExclusive;
+        public static void ChangeIsAutoDelete(bool isAutoDelete) => IsAutoDelete = isAutoDelete;
+        public static void ChangeArguments(IDictionary<string, object> arguments) => Arguments = arguments;
     }
 }
